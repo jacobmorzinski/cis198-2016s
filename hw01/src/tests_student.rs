e402a809b31986c75d14998d04a70dbee9170339
Jacob Morzinski <jmorzins@mit.edu>
2016-01-28 21:46:53 -0500

    more tests

--- hw01/src/tests_student.rs
+++ hw01/src/tests_student.rs
@@ -2,12 +2,22 @@
 
 use problem1::{sum};
 use problem2::{mat_mult};
+use problem3::{sieve};
+use problem4::{hanoi,Peg};
+
+//
+// Problem 1
+//
 
 #[test]
 fn test_sum() {
     let array = [1,1,2,3,5,8];
     assert_eq!(sum(&array), 20);
 }
+
+//
+// Problem 2
+//
 
 #[test]
 fn test_matmul() {
@@ -20,4 +30,26 @@ fn test_matmul() {
             assert_eq!(result[i][j], desired[i][j]);
         }
     }
+}
+
+//
+// Problem 3
+//
+
+#[test]
+fn test_sieve() {
+    assert_eq!(vec![2,3,5,7,11,13,17,19,21], sieve(22));
+}
+
+//
+// Problem 4
+//
+
+#[test]
+fn test_hanoi_2_disks() {
+    let result = hanoi(2, Peg::A, Peg::B, Peg::C);
+    assert_eq!(vec![(Peg::A, Peg::B),
+                    (Peg::A, Peg::C),
+                    (Peg::B, Peg::C)], result);
+    assert_eq!(3, result.len());
 }
